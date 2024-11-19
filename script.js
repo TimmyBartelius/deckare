f10e4bd26e8370782e5c10866542654d91a2a24c
Timmy <timmy.bartelius1@gmail.com>
2024-11-19 09:58:20 +0100

    Övning

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const cardGrid = document.querySelector('.card-grid')
 
 //console.log('Deckare', books, cardGrid)
 
+books.forEach(book =>{
+	const card = document.createElement('section')
+	const cardTitle = document.createElement('h3')
+	const cardAuthor = document.createElement('p')
+	const cardPrice = document.createElement('p')
+
+	card.classList.add('card')
+
+	cardTitle.innerText = book.title
+	cardAuthor.innerText = book.author
+	cardPrice.innterText = book.price
+
+	card.append(cardPrice)
+})
